d9dd3ff30a39f2b8f3e89c01c1c89abb750e1bdd
millkywaay <nkoirun10@gmail.com>
2025-06-12 08:28:09 +0700

    fix  bug

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -2,45 +2,16 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
-        $items = [
-            [
-                'kode_barang' => 'P001',
-                'nama_barang' => 'Buku Tulis',
-                'satuan' => 'pcs',
-                'stok_minimum' => 10,
-                'deskripsi' => 'Buku tulis 40 lembar untuk pelajar.',
-                'photo_product' => 'sup-game-box-400.png',
-                'category_id' => '4',
-            ],
-            [
-                'kode_barang' => 'P002',
-                'nama_barang' => 'Pulpen',
-                'satuan' => 'pcs',
-                'stok_minimum' => 20,
-                'deskripsi' => 'Pulpen tinta biru standar.',
-                'photo_product' => 'samsung-watch-4.png',
-                'category_id' => '4',
-            ],
-            [
-                'kode_barang' => 'P003',
-                'nama_barang' => 'Kertas A4',
-                'satuan' => 'box',
-                'stok_minimum' => 5,
-                'deskripsi' => 'Kertas A4 80 gsm isi 500 lembar.',
-                'photo_product' => 'samsung-s22.png',
-                'category_id' => 'Perlengkapan Kantor',
-            ],
-        ];
-
-        foreach ($items as $item) {
-            Item::create($item);
-        }
+        //
     }
 }
